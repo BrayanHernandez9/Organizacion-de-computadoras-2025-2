eb14cf0e7b7622f08cfb97a1d0c59b4c78cb945b
BrayanHernandez9 <brayan.hernandez44@uabc.edu.mx>
2025-10-17 22:57:18 +0000

    P6

--- P6/P6c.asm
+++ P6/P6c.asm
@@ -1,16 +1,25 @@
 %include "../LIB/pc_iox.inc"
 
+section .data
+N      dw 0
+
 section	.text
 
 	global _start       ;must be declared for using gcc
 
 _start:                     ;tell linker entry point
    
-
     mov ebx, 0x5C4B2A60
     ADD ebx, 0x02202783
     push bx 
+    
     mov eax, ebx
+    call pHex_dw
+
+    mov al, 8
+    mul bl
+    mov word [N], ax
+
     call pHex_dw
     
     mov al,10
@@ -19,3 +28,4 @@ _start:                     ;tell linker entry point
 
 	mov eax, 1	;system call number (sys_exit) -- fin del programa
 	int 0x80        ;call kernel
+
