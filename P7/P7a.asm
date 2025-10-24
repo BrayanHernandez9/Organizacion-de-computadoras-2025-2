8354b81da36e6d1b7954180981b709a908f9fffb
BrayanHernandez9 <brayan.hernandez44@uabc.edu.mx>
2025-10-24 23:04:35 +0000

    P7

--- P7/P7a.asm
+++ P7/P7a.asm
@@ -7,15 +7,34 @@ section	.text
 _start:                     ;tell linker entry point
 
     mov eax, 0x22446688 ; resultado esperado 0x82244668
-	rol eax, 6
-
-	;ROl eax, 2
-	;ror al, 2
-	;ror ah, 2
-	;rol al, 3
-	;rol [al], 1
+	
+	ror eax, 1
 	call pHex_dw
+	push eax
 
+	mov al,10	
+	call putchar
+
+	pop eax
+	ror eax, 1
+	call pHex_dw
+	push eax
+
+	mov al,10	
+	call putchar
+
+	pop eax
+	ror eax, 1
+	call pHex_dw
+	push eax
+
+	mov al,10	
+	call putchar
+
+	pop eax
+	ror eax, 1
+	call pHex_dw
+	
 	mov al,10	
 	call putchar
 
